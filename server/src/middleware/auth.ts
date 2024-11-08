9be4da0f1c63effe0915294be34ad0880d32c471
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-11-07 21:42:46 -0600

    filled in function based on example

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,20 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    const secret: any = process.env.JWT_SECRET_KEY;
+    // const secretKey = process.env.JWT_SECRET_KEY || '';
+    jwt.verify(token, secret, (error: any, user: any) => {
+      if (error){
+        return res.sendStatus(403);
+      }
+      req.user = user as JwtPayload;
+      return next();
+    })
+  } else {
+    return res.sendStatus(401);
+  }
+
 };
